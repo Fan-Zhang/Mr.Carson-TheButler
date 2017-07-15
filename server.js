6e85deeccf3e6d0f6467e4ded01cabf457d7c761
Fan Zhang <farlenzhang@gmail.com>
2017-07-14 23:56:30 -0700

    Make style works.

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ var fs = require('fs');
 var server = http.createServer(function (req, res) {
     fs.readFile(decodeURIComponent(req.url.substr(1)), function(err, data) {
         if (err === null) {
-            res.writeHead(200, { 
-                'Content-Type': 'text/html'
-            });
-
-        res.write(data);  // data is actually a buffer
+            res.writeHead(200, {});
+            res.write(data);  // data is actually a buffer
         }
 
         res.end();
     });
 });
 
-server.listen(8080);
-            
+server.listen(8088);
 
