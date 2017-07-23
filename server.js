e155709cb5ec3a6cd016aebf7ff82078d888d72d
Fan Zhang <farlenzhang@gmail.com>
2017-07-22 22:24:39 -0700

    Error handling.

--- server.js
+++ server.js
@@ -1,45 +1,58 @@
 // TODO: ERROR IN CONSOLE GET http://localhost:3000/app?name=slack net::ERR_CONNECTION_REFUSED
 const express = require('express');
 const server = express();
+const { execFile } = require('child_process');
 
 server.use(express.static('web'));
 
 server.get('/app', function (req, res) {
-    var child = require('child_process').execFile;
-    var clientInput = req.query.name;
-    var clientInputKeywords = clientInput.substring(clientInput.indexOf(' ')+1);
-    var cmd = req.query.config.cmd;
-    var parameters = [];
+    if (!(req.query.input && req.query.config && req.query.config.cmd)) {
+        res.send("Server: Invalid input or configuration.");
+        return;
+    }
 
-    if (/^(d|dic) /.test(clientInput)) {
-        parameters = [req.query.config.url + clientInputKeywords];
-    } else if (/^(s|slack)/.test(clientInput)) {
-        parameters = ['-a', 'Slack.app'];
+    var input = req.query.input;
+    var cmd = req.query.config.cmd;
+    var params = [];
+
+    if (/^(d|dic) /.test(input)) {
+        if (!req.query.config.url) {
+            res.send("Server: Invalid input or configuration.");
+            return;
+        }
+        var pluginInput = input.substring(input.indexOf(' ')+1);
+        params = [req.query.config.url + pluginInput];
+    } else if (/^(s|slack)/.test(input)) {
+        params = ['-a', 'Slack.app'];
     }
 
     // http://ourcodeworld.com/articles/read/154/how-to-execute-an-exe-file-system-application-using-electron-framework
-    child(cmd, parameters, function (err, data) {
+    execFile(cmd, params, function (err, stdout, stderr) {
         if (err) {
-            console.error(err);
-            return;
+            res.send('Server: Failed to open app.');
+        } else {
+            res.send('Server: Success!');
         }
-        console.log(data.toString());
     });
 });
 
 server.get('/search', function (req, res) {
-    var child = require('child_process').execFile;
-    var clientInput = req.query.name;
-    var cmd = 'find';
-    var parameters = ['/Users/fan/Documents', '-iname', '*'+clientInput+'*', '-atime', '-30', '-type', 'f'];
+    if (!(req.query.input && req.query.config && req.query.config.paths)) {
+        res.send("Server: Invalid input or configuration.");
+        return;
+    }
 
-    child(cmd, parameters, function (err, data) {
+    var input = req.query.input;
+    var cmd = 'find';
+    var params = req.query.config.paths.concat(
+        ['-iname', '*'+input+'*', '-atime', '-30', '-type', 'f']);
+
+    execFile(cmd, params, function (err, stdout, stderr) {
         if (err) {
-            console.error(err);
-            return;
+            res.send('Server: Failed to run find.');
+        } else {
+            res.send(stdout);
         }
-        console.log(data.toString());
-        res.send(data);
     });
 });
 
--- web/js/front.js
+++ web/js/front.js
@@ -10,8 +10,7 @@ jQuery(document).ready(function() {
             // on carriage return
             if (evt.which === 13) {
                 var input = jQuery(this).val();
-                var inputKeywords = input.substring(input.indexOf(' ')+1);
-                updateOutputDiv(dispatch(input, inputKeywords))
+                updateOutput(dispatch(input))
                 return false;
             }
         }
@@ -27,36 +26,59 @@ jQuery(document).ready(function() {
         function(evt) {
             evt.stopPropagation();
             evt.preventDefault();
-            var configFile = evt.originalEvent.dataTransfer.files[0];
-            var reader = new FileReader();
-            reader.onload = function(evt) {
-                config = JSON.parse(evt.target.result);
-            };
-            reader.readAsText(configFile);
+            if (evt.originalEvent.dataTransfer.files.length != 1) {
+                updateOutput('Only one JSON config file allowed.');
+            } else {
+                var configFile = evt.originalEvent.dataTransfer.files[0];
+                var reader = new FileReader();
+                reader.onload = function(evt) {
+                    try {
+                        config = JSON.parse(evt.target.result);
+                        updateOutput('Config file loaded successfully!');
+                        jQuery('#s').prop("disabled", false)
+                                    .val("How may I help you, sir/ma'am?")
+                                    .focus();
+                        jQuery('#config-drop-zone').toggle();
+                    } catch (err) {
+                        updateOutput(err.message);
+                    }
+                };
+                reader.readAsText(configFile);
+            }
             return false;
         }
     );
 });
 
-var config;
+var config = {};
 
-function dispatch(input, inputKeywords)
+function dispatch(input)
 {
+    // If no space's found, indexOf() returns -1,
+    // then pluginInput is the same as input
+    var pluginInput = input.substring(input.indexOf(' ')+1);
     for (var i = 0; i < plugins.length; i++) {
         var plugin = plugins[i];
         if (plugin.pattern.test(input)) {
-            var pluginConfig = config ?  config.PluginConfig[plugin.id] : {};
-            return plugin.action(
-                pluginConfig,
-                input,
-                inputKeywords,
-                updateOutputDiv
-            );
+            var pluginConfig = config.PluginConfig[plugin.id];
+            var output = '';
+            try {
+                output = plugin.action(
+                    pluginConfig,
+                    input,
+                    pluginInput,
+                    updateOutput
+                );
+            } catch (err) {
+                output = err.message;
+            }
+            return output;
         }
     }
+    return "No matching plugin.";
 }
 
-function updateOutputDiv(output) {
+function updateOutput(output) {
     jQuery('#output').text(output);
 }
 
@@ -70,7 +92,8 @@ var plugins = [
      *     - the substring after the first space
      *     - a callback function that is executed
      *       if an ajax call is made successfully.
-     * Action returns a string that servers as an immediate feedback.
+     * Action returns a string that serves as an immediate feedback.
+     * Action may throw exceptions.
      */
     { id: 'calculator',  pattern: /^= /,          action: calculator   },
     { id: 'google',      pattern: /^(g|google) /, action: googleSearch },
@@ -79,31 +102,43 @@ var plugins = [
     { id: 'file-search', pattern: /^'/,           action: fileSearch   },
 ];
 
-function calculator(config, input, inputKeywords, callback) {
-    // allowed symbols: + - * / % space () . 0-9
+function calculator(config, input, pluginInput, callback) {
+    // Allowed symbols: + - * / % space () . 0-9
     var regex = /^[+-\\*/\d%() .]*$/;
     var output = '';
-    if (regex.test(inputKeywords)) {
-        output = eval(inputKeywords);
+    if (regex.test(pluginInput)) {
+        output = eval(pluginInput);
     } else {
         output = 'Invalid expression';
     }
     return output;
 }
 
-function googleSearch(config, input, inputKeywords, callback) {
-    window.open('https://www.google.com/?q='+inputKeywords);
-    return 'Opening new window...';
+function googleSearch(config, input, pluginInput, callback) {
+    var newWindow = window.open('https://www.google.com/?q='+pluginInput);
+    if (newWindow) {
+        return 'Opening new window...';
+    } else {
+        return 'Something went wrong...';
+    }
 }
 
-function openApp(config, input, inputKeywords, callback) {
-    jQuery.get('/app', {name: input, config: config}, function(data, status) {});
-    return 'Opening ' + input + ' ...';
+function openApp(config, input, pluginInput, callback) {
+    jQuery.get('/app',
+               { input: input, config: config },
+               function(data, status) {
+                   callback(data);
+               }
+    );
+    return 'Opening app ...';
 }
 
-function fileSearch(config, input, inputKeywords, callback) {
-    jQuery.get('/search', {name: input.substring(1), config: config}, function(data, status) {
-        callback(data.toString());
-    });
+function fileSearch(config, input, pluginInput, callback) {
+    jQuery.get('/search',
+               { input: input.substring(1), config: config },
+               function(data, status) {
+                   callback(data);
+               }
+    );
     return 'Searching...';
 }
